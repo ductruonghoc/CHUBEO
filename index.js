b234f091a8a55c19ad8765c6bfeb4dff048a772a
ductruonghoc <92839932+ductruonghoc@users.noreply.github.com>
2024-05-21 18:23:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -107,6 +107,11 @@ var items = [
         "Price": 80
     },
     {
+        "Id": "THC",
+        "Name": "Thanh Cua",
+        "Price": 80
+    },
+    {
         "Id": "PLN",
         "Name": "Phi Lê Nai",
         "Price": 78
